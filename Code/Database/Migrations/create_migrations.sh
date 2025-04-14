eb80e2e5a6b6e34d69069917594de6d70a9b2d13
DanielFerreira-hub <142699926+DanielFerreira-hub@users.noreply.github.com>
2025-04-14 12:31:10 +0100

    Create create_migrations.sh

---
+++ Code/Database/Migrations/create_migrations.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+php artisan make:migration create_users_table
+php artisan make:migration create_roles_table
+php artisan make:migration create_assets_table
+php artisan make:migration create_categories_table
+php artisan make:migration create_assignments_table
+php artisan make:migration create_maintenance_logs_table
+php artisan make:migration create_locations_table
+php artisan make:migration create_suppliers_table
+php artisan make:migration create_rooms_table
