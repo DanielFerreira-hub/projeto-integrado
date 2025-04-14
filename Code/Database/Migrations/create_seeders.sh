667e7e8f908cb8eae94bfe87b955c6df36dbf0c9
DanielFerreira-hub <142699926+DanielFerreira-hub@users.noreply.github.com>
2025-04-14 14:58:18 +0100

    Update create_seeders.sh

--- Code/Database/Migrations/create_seeders.sh
+++ Code/Database/Migrations/create_seeders.sh
@@ -3,3 +3,6 @@ php artisan make:seeder RolesTableSeeder
 
 # Create seeder for the Rooms table
 php artisan make:seeder RoomsTableSeeder
+
+# Create seeder for the Categories table
+php artisan make:seeder CategoriesTableSeeder
