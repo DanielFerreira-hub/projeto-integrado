145e92dc24d453981d6ee36cffc83e0d4c0e2a65
DanielFerreira-hub <142699926+DanielFerreira-hub@users.noreply.github.com>
2025-04-23 15:32:36 +0100

    Create UserController.php

---
+++ Code/Backend/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function getAllUsers()
+    {
+        return response()->json(User::all());
+    }
+
+    public function createUser(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+        ]);
+
+        return response()->json($user, 201);
+    }
+}
